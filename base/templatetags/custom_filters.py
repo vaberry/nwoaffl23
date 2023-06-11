427e6c887bb24324fa0e471e06a96d03ed05f39d
vaberry <vincent.berry11@gmail.com>
2023-06-11 14:16:11 -0400

    pics

--- base/templatetags/custom_filters.py
+++ base/templatetags/custom_filters.py
@@ -5,3 +5,11 @@ register = template.Library()
 @register.filter
 def create_range(value):
     return range(1, int(value) + 1)
+
+@register.filter
+def get_dict_value(dictionary, key):
+    # print(dictionary)
+    # print(key)
+    # show = dictionary.get(str(key), None)
+    # print(show)
+    return dictionary.get(str(key), None)
--- nwoaffl23/context_processors.py
+++ nwoaffl23/context_processors.py
@@ -1,6 +1,22 @@
 from base.models import Team
 
 def navbar_context(request):
+    pic_dict = {
+        'jakeyoder' : 'base/img/profile_pics/amish.jpeg',
+        'sharonblair' : 'base/img/profile_pics/brady.jpeg',
+        'bobsmith' : 'base/img/profile_pics/bruisers.gif',
+        'jaysoncromly' : 'base/img/profile_pics/dawgs.gif',
+        'ericblair' : 'base/img/profile_pics/force.jpg',
+        'mattsevey' : 'base/img/profile_pics/mattimine.gif',
+        'tellyfricke' : 'base/img/profile_pics/secrets.png',
+        'vinceberry' : 'base/img/profile_pics/specials.png',
+        'robfricke' : 'base/img/profile_pics/tricksters.gif',
+        'jacobcromly' : 'base/img/profile_pics/twisters.gif',
+        'trevorblair' : 'base/img/profile_pics/tycoons.jpeg',
+        'troyblair' : 'base/img/profile_pics/warriors.jpeg',
+        'team13' : 'base/img/profile_pics/team-13.png',
+        'team14' : 'base/img/profile_pics/team-14.png',
+    }
     user_team_pk = None
     user_team = None
     commissioner = None
@@ -14,6 +30,7 @@ def navbar_context(request):
         commissioner = Team.objects.filter(commissioner=True).first()
 
     context = {
+        'pic_dict': pic_dict,
         'commissioner': commissioner.owner if commissioner else None,
         'user_team_pk': user_team_pk,
         'teams': Team.objects.all().order_by('draft_order'),
