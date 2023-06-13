575ddb0cd0d3ab84a6350c3860350d858c2cbfbe
vaberry <vincent.berry11@gmail.com>
2023-06-13 12:04:01 -0400

    e

--- nwoaffl23/urls.py
+++ nwoaffl23/urls.py
@@ -7,7 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('base.urls')),
-] 
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
